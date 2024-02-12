d1e1af0cc657336deba870786ec47371642f28c0
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2024-02-12 04:38:39 -0800

    Added multiple tag types for visibility

--- src/lib/cards/node/NodeTags.svelte
+++ src/lib/cards/node/NodeTags.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { Node } from '$lib/common/types';
-	import { onMount } from 'svelte';
 	import CardListEntry from '../CardListEntry.svelte';
 	import { InputChip, getToastStore } from '@skeletonlabs/skeleton';
 	import { NodeStore, updateStoreItem } from '$lib/Stores';
@@ -9,7 +8,9 @@
 
 	export let node: Node;
 
-	$: tags = node.forcedTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsForced = node.forcedTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsValid = node.validTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsInvalid = node.invalidTags.map((tag) => tag.replace('tag:', ''));
 	$: disabled = false;
 
 	const ToastStore = getToastStore();
@@ -17,25 +18,43 @@
 	async function saveTags() {
 		disabled = true;
 		try {
-			const n = await setNodeTags(node, tags);
+			const n = await setNodeTags(node, tagsForced);
+			n.validTags = [...tagsValid];
+			n.invalidTags = [...tagsInvalid];
 			await updateStoreItem(NodeStore, n);
 		} catch (e) {
 			toastError('Invalid Tags: ' + e, ToastStore);
 		} finally {
-			tags = node.forcedTags.map((tag) => tag.replace('tag:', ''));
+			tagsForced = node.forcedTags.map((tag) => tag.replace('tag:', ''));
 			disabled = false;
 		}
 	}
 </script>
 
-<CardListEntry top title="Tags:">
-	<InputChip
-		name="tags-node-{node.id}"
-		{disabled}
-		value={tags}
-		class="w-full"
-		chips="variant-filled-success"
-		on:add={saveTags}
-		on:remove={saveTags}
-	/>
-</CardListEntry>
+<div class="space-y-4">
+	<CardListEntry top title="Tags:">
+		<InputChip
+			name="tags-forced-node-{node.id}"
+			{disabled}
+			value={tagsForced}
+			class="w-full"
+			chips="variant-filled-success"
+			on:add={saveTags}
+			on:remove={saveTags}
+		/>
+	</CardListEntry>
+	<CardListEntry top title="Assigned Tags:">
+		<div class="space-x-2 space-y-1">
+			{#each tagsValid as tag}
+				<button type="button" class="chip variant-filled-success">{tag}</button>
+			{/each}
+		</div>
+	</CardListEntry>
+	<CardListEntry top title="Prevented Tags:">
+		<div class="space-x-2 space-y-1">
+			{#each tagsInvalid as tag}
+				<button type="button" class="chip variant-filled-error">{tag}</button>
+			{/each}
+		</div>
+	</CardListEntry>
+</div>
