18ddade0a8fa14b6374fa15ac281ed4dd2a8e322
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2024-02-12 04:35:52 -0800

    Added additional tag types for visibility

--- src/lib/cards/node/NodeTags.svelte
+++ src/lib/cards/node/NodeTags.svelte
@@ -6,12 +6,13 @@
 	import { NodeStore, updateStoreItem } from '$lib/Stores';
 	import { setNodeTags } from '$lib/common/api';
 	import { toastError } from '$lib/common/funcs';
+	import { invalidate } from '$app/navigation';
 
 	export let node: Node;
 
-	$: tags = node.forcedTags.map((tag) => tag.replace('tag:', ''));
-	$: tags2 = node.validTags.map((tag) => tag.replace('tag:', ''));
-	$: tags3 = node.invalidTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsForced = node.forcedTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsValid = node.validTags.map((tag) => tag.replace('tag:', ''));
+	$: tagsInvalid = node.invalidTags.map((tag) => tag.replace('tag:', ''));
 	$: disabled = false;
 
 	const ToastStore = getToastStore();
@@ -19,39 +20,43 @@
 	async function saveTags() {
 		disabled = true;
 		try {
-			const n = await setNodeTags(node, tags);
+			const n = await setNodeTags(node, tagsForced);
+			n.validTags = [...tagsValid];
+			n.invalidTags = [...tagsInvalid];
 			await updateStoreItem(NodeStore, n);
 		} catch (e) {
 			toastError('Invalid Tags: ' + e, ToastStore);
 		} finally {
-			tags = node.forcedTags.map((tag) => tag.replace('tag:', ''));
+			tagsForced = node.forcedTags.map((tag) => tag.replace('tag:', ''));
 			disabled = false;
 		}
 	}
 </script>
 
-<CardListEntry top title="Tags:">
-	<InputChip
-		name="tags-node-{node.id}"
-		{disabled}
-		value={tags}
-		class="w-full"
-		chips="variant-filled-success"
-		on:add={saveTags}
-		on:remove={saveTags}
-	/>
-	<InputChip
-		name="tags2-node-{node.id}"
-		disabled
-		value={tags2}
-		class="w-full mt-2"
-		chips="variant-filled-success"
-	/>
-	<InputChip
-		name="tags3-node-{node.id}"
-		disabled
-		value={tags3}
-		class="w-full mt-2"
-		chips="variant-filled-success"
-	/>
-</CardListEntry>
+<div class="space-y-4">
+	<CardListEntry top title="Tags:">
+		<InputChip
+			name="tags-forced-node-{node.id}"
+			{disabled}
+			value={tagsForced}
+			class="w-full"
+			chips="variant-filled-success"
+			on:add={saveTags}
+			on:remove={saveTags}
+		/>
+	</CardListEntry>
+	<CardListEntry top title="Assigned Tags:">
+		<div class="space-x-2 space-y-1">
+			{#each tagsValid as tag}
+				<button type="button" class="chip variant-filled-success">{tag}</button>
+			{/each}
+		</div>
+	</CardListEntry>
+	<CardListEntry top title="Prevented Tags:">
+		<div class="space-x-2 space-y-1">
+			{#each tagsInvalid as tag}
+				<button type="button" class="chip variant-filled-error">{tag}</button>
+			{/each}
+		</div>
+	</CardListEntry>
+</div>
