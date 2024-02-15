b9c1d95209afcbe518598061e2c6fa050a3404a2
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2024-02-15 13:43:36 -0800

    version bump

--- src/lib/common/debug.ts
+++ src/lib/common/debug.ts
@@ -1,7 +1,7 @@
 import { DebugStore } from '$lib/Stores';
 import { get } from 'svelte/store';
 
-export const version = '0.1.9b';
+export const version = '0.1.10b';
 
 export function debug(...data: unknown[]) {
 	// output if console debugging is enabled
