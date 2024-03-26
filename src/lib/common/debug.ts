235441f7d29d1cc99719a92e0276f8e0fe1e7c2d
GoodiesHQ <GoodiesHQ>
2024-03-25 18:28:33 -0700

    Set version v0.1.11b

--- src/lib/common/debug.ts
+++ src/lib/common/debug.ts
@@ -1,7 +1,7 @@
 import { DebugStore } from '$lib/Stores';
 import { get } from 'svelte/store';
 
-export const version = 'dev';
+export const version = 'v0.1.11b';
 
 export function debug(...data: unknown[]) {
 	// output if console debugging is enabled
