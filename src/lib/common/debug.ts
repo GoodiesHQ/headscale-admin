dcbf962d9fb335af5f84a3880e53e9dba5a05c1d
GoodiesHQ <aarcher73k@gmail.com>
2025-01-19 01:44:37 -0800

    bump to version 0.24.2

--- src/lib/common/debug.ts
+++ src/lib/common/debug.ts
@@ -1,6 +1,6 @@
 import { App } from "$lib/States.svelte";
 
-export const version = '0.24.1';
+export const version = '0.24.2';
 
 export function debug(...data: unknown[]) {
 	// output if console debugging is enabled
